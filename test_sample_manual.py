624e9140b78d13b48f25eed982f4fd315634c79a
raja-m-caspar-ai <rajashekhar.m@caspar.ai>
2019-09-24 13:11:25 +0530

    added one script

---
+++ test_sample_manual.py
@@ -0,0 +1,16 @@
+#from pytest import XRAY_API_CLIENT_ID
+#from pytest import XRAY_API_CLIENT_SECRET
+#from pytest import XRAY
+#os.environ["XRAY_API_CLIENT_ID"] = "CA1B7A745758443D93E381F0C7BFE9BC"
+#os.environ["XRAY_API_CLIENT_SECRET"] = "73678d088908a58c995e10ad75f6af17c940b12666959c3850ebcb58e98af693"
+
+import pdb
+import pytest
+import pytest_xray
+
+@pytest.mark.xray(test_key="DT-1", test_exec_key="DT-1")
+def test_my_function():
+    assert True == True
+
+
+
